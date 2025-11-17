132e97f252f5d7e52005dffa85c034e3baec0ffd
nk3unico1533 <nk3unico@outlook.com>
2025-11-17 10:25:12 -0300

    Criar page.js

---
+++ app/consulta/page.js
@@ -0,0 +1,8 @@
+export default function ConsultaPage() {
+  return (
+    <div className="card-large">
+      <h1 className="title">Consulta 🔎</h1>
+      <p>Faça consultas no sistema. (Conteúdo será integrado ao backend.)</p>
+    </div>
+  );
+}
