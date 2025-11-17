269241cad38cb42890b34fa9f3f3a100306d22d8
nk3unico1533 <nk3unico@outlook.com>
2025-11-17 10:25:36 -0300

    Criar page.js

---
+++ app/dashboard/page.js
@@ -0,0 +1,8 @@
+export default function DashboardPage() {
+  return (
+    <div className="card-large">
+      <h1 className="title">Dashboard 📊</h1>
+      <p>Veja gráficos, estatísticas e indicadores importantes.</p>
+    </div>
+  );
+}
